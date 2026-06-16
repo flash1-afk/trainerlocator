b81d48073a0c47aa0a75d729e9d41dbcdf703c52
flash1-afk <heaterheater885@gmail.com>
2026-06-17 00:13:53 +0500

    Fix Avatar DB path to use /tmp on Vercel

--- copy_avatar_new1/backend/src/db/database.js
+++ copy_avatar_new1/backend/src/db/database.js
@@ -2,7 +2,7 @@ const Database = require('better-sqlite3');
 const path      = require('path');
 const fs        = require('fs');
 
-const DB_PATH = process.env.DB_PATH || path.join(__dirname, '../../data/avatar_buddy.db');
+const DB_PATH = process.env.DB_PATH || (process.env.VERCEL ? '/tmp/avatar_buddy.db' : path.join(__dirname, '../../data/avatar_buddy.db'));
 
 let db;
 
